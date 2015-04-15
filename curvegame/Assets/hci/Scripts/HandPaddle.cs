5ed16105b905e55f58b9bea5859f55649fee8ad8
mania101 <gmarcomaniaci@aol.com>
2015-04-15 14:20:36 -0700

    Paddle Follows Hand
    Paddle now follows pointer finger. Check HandPaddle Script I made if
    you want to make any changes.

---
+++ curvegame/Assets/hci/Scripts/HandPaddle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class HandPaddle : MonoBehaviour {
+	public HandModel m_handModel;
+	public GameObject playerPaddle;
+
+	// Use this for initialization
+	void Start () {
+		m_handModel = transform.GetComponent<HandModel>();
+		playerPaddle = GameObject.Find ("PlayerPaddle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 indexPosition = m_handModel.fingers [1].GetTipPosition ();
+		Debug.Log (indexPosition);
+//		Vector3 localPosition = indexPosition.TipPosition.toUnityScaled ();
+		playerPaddle.transform.position = indexPosition;
+	}
+}
