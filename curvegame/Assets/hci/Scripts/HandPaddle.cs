6a31c8827aa6f36d6736d91d0cd1da6500fb029b
mania101 <gmarcomaniaci@aol.com>
2015-04-23 11:07:51 -0700

    Final Marco Push
    Menu and everything else is set up
    If you guys can figure out the lighting issue… great, if not just use
    the 5 lights i have in the rest of the scenes after menu

--- curvegame/Assets/hci/Scripts/HandPaddle.cs
+++ curvegame/Assets/hci/Scripts/HandPaddle.cs
@@ -10,12 +10,17 @@ public class HandPaddle : MonoBehaviour {
 	void Start () {
 		m_handModel = transform.GetComponent<HandModel>();
 		playerPaddle = GameObject.Find ("PlayerPaddle");
+//		GameObject lightGameObject = new GameObject("The Light");
+//		Light lightComp = lightGameObject.AddComponent<Light>();
+//		lightComp.color = Color.blue;
+//		lightGameObject.transform.position = new Vector3(2.34, 5.43, 0);
+//		lightGameObject.transform.rotation = new Vector3 (50, 330, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 indexPosition = m_handModel.fingers [1].GetTipPosition ();
-		Debug.Log (indexPosition);
+		//Debug.Log (indexPosition);
 //		Vector3 localPosition = indexPosition.TipPosition.toUnityScaled ();
 		playerPaddle.transform.position = indexPosition;
 	}
---
+++ curvegame/Assets/hci/Scripts/StartGame.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+	GameObject startGame, startHow, startCredits;
+	public int boxOne;
+	// Use this for initialization
+	void Start () {
+		startGame = GameObject.Find ("Start");
+		startHow = GameObject.Find ("HowToPlay");
+		startCredits = GameObject.Find ("Credits");
+
+	
+	}
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("you made it here");
+		if (boxOne == -1) {
+			Application.LoadLevel (0);
+			Debug.Log("Start");
+		}
+		if (boxOne == 0) {
+			//Application.LoadLevel (2);
+			Debug.Log("How to play");
+		}
+		if (boxOne == 1) {
+			//Application.LoadLevel (3);
+			Debug.Log("Credits");
+			Application.LoadLevel (1);
+		}
+	}
+//	void OnCollisionEnter(Collision col){
+//		if ("Start" == col.gameObject.tag) {
+//			Application.LoadLevel (1);
+//			Debug.Log("Start");
+//		}
+//		if (col.gameObject.tag == "HowToPlay") {
+//			//Application.LoadLevel (2);
+//			Debug.Log("How to play");
+//		}
+//		if (col.gameObject.tag == "Credits") {
+//			//Application.LoadLevel (3);
+//			Debug.Log("Credits");
+//		}
+//	}
+
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
